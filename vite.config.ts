60330f3c525b1ef8749eb502c9a42531a94467e9
DengNaiChen <dengnaichen@gmail.com>
2025-12-25 22:06:53 -0500

    feat: setup environment separation for local and production

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'https://aether-web-372668020909.northamerica-northeast2.run.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/v1/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: env.VITE_API_PROXY_TARGET || 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/v1/, ''),
+        },
       },
     },
-  },
+  }
 })
